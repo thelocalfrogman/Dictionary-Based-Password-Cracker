6b3e9f316331c7cee19d11fd74f38279f9845051
Thauum <wochaston@gmail.com>
2025-06-18 13:36:23 +1000

    Update Hash-Dictionary-input.py
    Finishes dictionary/hash file input.

--- Hash-Dictionary-input.py
+++ Hash-Dictionary-input.py
@@ -4,11 +4,13 @@
 
 #Prompts the user for the file containing the hashes, and the dictionary of words.
 
-import os
 from pathlib import Path
 
-def whatpath():
-    file_path = input("Please enter the file path: ")
+file_path = 'a'
+dic_path = 'a'
+
+def whathash(file_path):
+    file_path = input("Please enter the hashes file path: ")
 
     path = Path(file_path)
     if path.exists():
@@ -16,8 +18,24 @@ def whatpath():
         file_name = file_path
         file_extension = Path(file_name).suffix
         print(f"The file extension is: {file_extension}")
+        return file_path
     else:
         print(f"The file path '{file_path}' does not exist. Please check and try again.")
-        whatpath()
+        whathash()
 
-whatpath()
+def whatdic(dic_path):
+    dic_path = input("Please enter the dictionary file path: ")
+
+    path = Path(dic_path)
+    if path.exists():
+        print(f"The file path '{dic_path}' is valid.")
+        file_name = dic_path
+        file_extension = Path(file_name).suffix
+        print(f"The file extension is: {file_extension}")
+        return dic_path
+    else:
+        print(f"The file path '{dic_path}' does not exist. Please check and try again.")
+        whatdic()
+
+file_path = whathash(file_path)
+dic_path = whatdic(dic_path)
